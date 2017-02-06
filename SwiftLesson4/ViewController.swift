0223a2d5c889185348756072cf0cf2199edaddc0
ortonomy <dev@ortonomy.co>
2017-02-06 12:17:10 +0800

    Initial Commit

---
+++ SwiftLesson4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLesson4
+//
+//  Created by Gregory Orton on 06/02/2017.
+//  Copyright © 2017 ortonomy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
